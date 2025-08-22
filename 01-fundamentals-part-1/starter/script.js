5a513a66d522ee43ce1d70bc65028eaf8ab32be0
annabellefrancineuy <uy.annabellefrancine@gmail.com>
2025-08-22 22:22:30 +0800

    feat: add conditional statements and string literals
    - Implement ES6 template literals with backticks for elegant string formatting
    - Add if/else control structures for decision-making logic
    - Create truthy/falsy value understanding with practical examples
    - Complete BMI Comparison challenge with intelligent output messages
    - Combine mathematical operations with conditional logic for real-world applications

--- 01-fundamentals-part-1/starter/script.js
+++ 01-fundamentals-part-1/starter/script.js
@@ -1,73 +1,73 @@
 // JavaScript Fundamentals - Part 1
 // We'll write our code here!
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
-console.log("=== VARIABLES ===");
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+// console.log("=== VARIABLES ===");
 
-let firstname = "Jonas";
-console.log(firstname);
+// let firstname = "Jonas";
+// console.log(firstname);
 
-let age = 30;
-console.log(age);  
+// let age = 30;
+// console.log(age);  
 
-age = 39;
-console.log(age);  
+// age = 39;
+// console.log(age);  
  
-const birthYear = 1991;
-console.log(birthYear);
+// const birthYear = 1991;
+// console.log(birthYear);
  
-const PI = 3.1415;
-console.log(PI);
+// const PI = 3.1415;
+// console.log(PI);
  
-//do not use var
-var job = "programmer";
+// //do not use var
+// var job = "programmer";
  
-//Good variable names
-let lastName = "Sarah";
-let myCurrentJob = "teacher";
-let PIZ = 3.14;
+// //Good variable names
+// let lastName = "Sarah";
+// let myCurrentJob = "teacher";
+// let PIZ = 3.14;
  
 //bad variable names
 //let 3Years
 //let jonas&matilda
 //let new
  
-const country = "Portugal"
-const language = "Portuguese"
-const population = 10;
+// const country = "Portugal"
+// const language = "Portuguese"
+// const population = 10;
  
-//use let only when you need you need to change the value
-let ageNow = 25;
-ageNow = 26;
+// //use let only when you need you need to change the value
+// let ageNow = 25;
+// ageNow = 26;
  
-console.log("=== DATA TYPES ===");
+// console.log("=== DATA TYPES ===");
  
-let weight = 45;
-console.log(weight); 45
-console.log(typeof age); 'number'
+// let weight = 45;
+// console.log(weight); 45
+// console.log(typeof age); 'number'
  
-//String
-let username = "";
-console.log(username);
-console.log(typeof username);
+// //String
+// let username = "";
+// console.log(username);
+// console.log(typeof username);
  
-//Boolean
-let javascriptisfun = true;
-console.log(javascriptisfun);
-console.log(typeof javascriptisfun);
+// //Boolean
+// let javascriptisfun = true;
+// console.log(javascriptisfun);
+// console.log(typeof javascriptisfun);
  
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
  
-let dynamicVariable = 23;
-console.log(dynamicVariable, typeof dynamicVariable)
+// let dynamicVariable = 23;
+// console.log(dynamicVariable, typeof dynamicVariable)
  
-dynamicVariable = "Now i am a string";
-console.log(dynamicVariable, typeof dynamicVariable);
+// dynamicVariable = "Now i am a string";
+// console.log(dynamicVariable, typeof dynamicVariable);
  
-dynamicVariable = "true";
-console.log(dynamicVariable, typeof dynamicVariable);
+// dynamicVariable = "true";
+// console.log(dynamicVariable, typeof dynamicVariable);
  
 //////////////////////////
  
@@ -147,18 +147,18 @@ console.log(dynamicVariable, typeof dynamicVariable);
 
 //Operator precedence
  
-const now = 2037;
-const ageJonas = now -1991;
-const ageSarah = now - 2018;
+// const now = 2037;
+// const ageJonas = now -1991;
+// const ageSarah = now - 2018;
 
-console.log(now - 1991 > now - 2018);
+// console.log(now - 1991 > now - 2018);
 
-let x, y;
-x = y = 25 - 10 - 5; 
-console.log(x, y);
+// let x, y;
+// x = y = 25 - 10 - 5; 
+// console.log(x, y);
 
-const averageAge = (ageJonas + ageSarah) /2;
-console.log(ageJonas, ageSarah, averageAge);
+// const averageAge = (ageJonas + ageSarah) /2;
+// console.log(ageJonas, ageSarah, averageAge);
 
 //Test Data 1
 let massMark = 78;
@@ -166,8 +166,8 @@ let heightMark = 1.69;
 let massJohn = 92;
 let heightJohn = 1.95;
  
-const bmiMark = massMark / heightMark **2; 
-const bmiJohn = massJohn / heightJohn **2;
+let bmiMark = massMark / heightMark **2; 
+let bmiJohn = massJohn / heightJohn **2;
 
 console.log("Mark's BMI:" +bmiMark);
 console.log("John's BMI:" + bmiJohn);
@@ -182,12 +182,45 @@ heightMark = 1.88;
 massJohn = 85;
 heightJohn = 1.76;
  
-const bmiMark2 = massMark / (heightMark * heightMark);
-const bmiJohn2 = massJohn / (heightJohn * heightJohn);
+bmiMark = massMark / (heightMark * heightMark);
+bmiJohn = massJohn / (heightJohn * heightJohn);
 
-console.log("Mark's BMI:" +bmiMark2);
-console.log("John's BMI:" + bmiJohn2);
-markHigherBMI = (bmiMark2 > bmiJohn2);
+console.log("Mark's BMI:" +bmiMark);
+console.log("John's BMI:" + bmiJohn);
+
+ markHigherBMI = (bmiMark > bmiJohn);
 
 console.log("Mark has a higher BMI than John:" + markHigherBMI);
+ 
+////////////////////////////////////
+// Strings and Template Literals
+// const firstName = "Jonas";
+// const job = "teacher";
+// const birthYear = 1991;
+// const year = 2037;
+ 
+// const jonas =
+//   "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
+// console.log(jonas);
+ 
+// const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}`;
+// console.log(jonasNew);
+
+//New BMI Calculator
+massMark = 78;
+heightMark = 1.69;
+massJohn = 92;
+heightJohn = 1.95;
+ 
+bmiMark = massMark / heightMark ** 2;
+bmiJohn = massJohn / (heightJohn * heightJohn);
+console.log(bmiMark, bmiJohn);
+ 
+if (bmiMark > bmiJohn) {
+    console.log(`Mark's BMI ${bmiMark} is higher than John's ${bmiJohn}!`);
+} else {
+    console.log(`John's BMI ${bmiJohn} is higher than Mark's ${bmiMark}!`);
+}
+
+
  
